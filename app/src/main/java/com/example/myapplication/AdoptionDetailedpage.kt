f4d28f3f6f02a17b5892826ecb4fc657fb18af69
Lord617 <noyongju@naver.com>
2024-02-04 17:15:49 +0900

    우리의 동행 페이지 접속 오류 해결/ 봉사 후기 클릭 시 앱이 꺼짐 이 부분만 해결하면 될 듯

--- app/src/main/java/com/example/myapplication/AdoptionDetailedpage.kt
+++ app/src/main/java/com/example/myapplication/AdoptionDetailedpage.kt
@@ -2,21 +2,41 @@ package com.example.myapplication
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import android.view.MenuItem
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
 class AdoptionDetailedpage: AppCompatActivity() {
-    lateinit var btnBack: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_adoption_detailedpage)
 
-        btnBack.setOnClickListener {
-            // 뒤로 가기 버튼 클릭 시 AdoptionView로 이동하는 코드
-            val intent = Intent(this, AdoptionView::class.java)
-            startActivity(intent)
+        // 액션바 뒤로가기
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        // 액션바 글자 색 변경
+        val spannableString = SpannableString("입양 후기")
+        spannableString.setSpan(
+            ForegroundColorSpan(ContextCompat.getColor(this, R.color.black)),
+            0, spannableString.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        supportActionBar?.title = spannableString
+    }
+
+    // 액션바 뒤로가기 기능 구현
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item?.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+            else -> {
+                return super.onOptionsItemSelected(item)
+            }
         }
+    }
 
     }
-}
